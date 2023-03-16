00cee77a4822c5c90727b39f73d1a97d94412eb8
lazyperson0710 <51500566+lazyperson0710@users.noreply.github.com>
2023-03-16 15:57:42 +0900

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,29 @@
+<?php
+
+$pdo = new PDO(
+	'sqlite:DataBase.db'
+);
+
+$pdo->query('CREATE TABLE IF NOT EXISTS DataBase (AAA TEXT, Time TIMESTAMP)');
+for ($i = 0; $i < 100; $i++) {
+	$cache[] = [
+		'AAA' => mt_rand(1000000000, 9999999999),
+		'time' => unixtojd(),
+	];
+}
+
+
+$pdo->exec('begin');
+try {
+	foreach ($cache as $data) {
+		$pdo->query("INSERT INTO DataBase VALUES(\"$data[AAA]\",  \"$data[time]\")");
+	}
+	$pdo->exec('commit');
+	echo "データがコミットされました";
+	return;
+} catch (PDOException $e) {
+	$pdo->exec('rollback');
+	echo "データがロールバックされました";
+	echo "Error: " . $e->getMessage();
+	return;
+}
